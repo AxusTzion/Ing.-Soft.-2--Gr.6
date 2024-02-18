d9e0dc8964ef0d362cce56a426cbe561a40419fc
BrayanOvalle <BrayanOvalle@DESKTOP-K88GCJO>
2024-02-18 13:06:35 -0500

    Add master

---
+++ src/main/java/com/creditapplication/CreditApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.creditapplication;
+
+/**
+ *
+ * @author BrayanOvalle
+ */
+public class CreditApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
