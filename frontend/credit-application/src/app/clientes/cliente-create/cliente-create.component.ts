2ee1fd372f86d8ee78c17bcdc4cf14e8a05cc22e
Juan <jvelascov1@ucentral.edu.co>
2024-04-09 21:24:24 -0500

    Add front end

---
+++ frontend/credit-application/src/app/clientes/cliente-create/cliente-create.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import {MatIcon} from "@angular/material/icon";
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {merge} from 'rxjs';
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+
+@Component({
+  selector: 'app-cliente-create',
+  standalone: true,
+  imports: [
+    MatFormField,
+    MatIcon,
+    ReactiveFormsModule,
+    MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule
+  ],
+  templateUrl: './cliente-create.component.html',
+  styleUrl: './cliente-create.component.css'
+})
+export class ClienteCreateComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  errorMessage = '';
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage = 'You must enter a value';
+    } else if (this.email.hasError('email')) {
+      this.errorMessage = 'Not a valid email';
+    } else {
+      this.errorMessage = '';
+    }
+  }
+}
