516cf555aef7d8492e8ed6c034603015cff92332
Juan <jvelascov1@ucentral.edu.co>
2024-04-16 21:05:24 -0500

    Add front end

--- frontend/credit-application/src/app/app.routes.ts
+++ frontend/credit-application/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import {ClienteCreateComponent} from "./clientes/cliente-create/cliente-create.component";
 
 export const routes: Routes = [
-  {path: '', component:ClienteCreateComponent}
+  {path: 'createclient', component:ClienteCreateComponent},
+  {path: 'listarCredito', component:ClienteCreateComponent}
 ];
